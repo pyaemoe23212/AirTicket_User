910cf1b68b909133f94c17afeee538aadb696e90
pyaemoe23212 <tunbhonepyaemoe@gmail.com>
2026-01-07 12:44:44 +0700

    first commit

---
+++ src/Pages/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 class="text-3xl text-yellow-500">Hello World</h1>
+    </>
+  )
+}
+
+export default App

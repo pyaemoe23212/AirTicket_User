a5bd571694d997337d7d606730c26802064fbb3d
pyaemoe23212 <tunbhonepyaemoe@gmail.com>
2026-02-18 16:10:48 +0630

    Fix User

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,27 @@
-import FlightSearchPage from "./FlightSearchPage";
+import HeroSection from "../components/HeroSection"
+import SearchForm from "../components/SearchForm"
+import AboutSection from "../components/AboutSection"
+import {useState} from "react"
+import { useNavigate } from "react-router"
 
-const Home = () => {
-  return <FlightSearchPage initialTripType="round-trip" />;
-};
+
+function Home(){
+  const navigate = useNavigate();
+  const [tripType, setTripType] = useState("round-trip");
+  
+  const handleSearch = (searchData) => {
+    navigate("/departure", {
+      state: { tripType, searchData },
+    });
+  };
+
+  return(
+    <div className="min-h-screen bg-gray-50">
+      <HeroSection />
+      <SearchForm tripType={tripType} onTripTypeChange={setTripType} onSearch={handleSearch} />
+      <AboutSection />
+    </div>
+  )
+}
 
 export default Home;
